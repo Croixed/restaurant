f5338e57eb1faa53c0c398efd35cca5632982ed1
tommy <bt@g>
2022-03-02 19:00:15 -0800

    added dash svg

--- src/home.js
+++ src/home.js
@@ -1,4 +1,5 @@
 import Steak from './steak.png';
+import Dash from './dash.svg';
 
 /* const mySteak = new Image();
 mySteak.src = Steak; */
@@ -20,13 +21,15 @@ export const homeGen = () => {
   mySteak.classList.add("home-img");
   const mySteak2 = mySteak.cloneNode(true);
 
+  const myDash = new Image();
+  myDash.src = Dash;
+
   const mainContent = component('div', '', 'main-content');
   const contentTitle = component('div', 'Welcome to Vegan Steaks', 'content-title')
   const contentText = component('div', 'Home of the only Vegan Steaks! How are our steaks vegan? We taught our cattle to only eat organic, ethically sourced, vegan foods. It took many years of training and genetic modifications using CRISPR to reach this point, and now we are willing to sell you the results!', 'content-text');
   const homeContent = component('div', '', 'home-content');
-  const doorDash = component('button', 'order on DoorDash', 'door-button')
   
-  homeContent.append(contentTitle, contentText, doorDash, mySteak, mySteak2)
+  homeContent.append(contentTitle, contentText, myDash, mySteak, mySteak2)
   mainContent.appendChild(homeContent);
   return mainContent
 }
